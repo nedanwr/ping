cd874eb0f81d9696f4f4d76d1eb4bd9b4d2cfd9b
Naveed <46922100+nedanwr@users.noreply.github.com>
2025-04-12 13:55:41 +0800

    (Chore): initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import { Geist } from "next/font/google";
+
+import { TRPCReactProvider } from "~/trpc/react";
+import "~/styles/globals.css";
+
+const geist = Geist({
+  subsets: ["latin"],
+  variable: "--font-geist-sans",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en" className={`${geist.variable}`}>
+      <body>
+        <TRPCReactProvider>{children}</TRPCReactProvider>
+      </body>
+    </html>
+  );
+}
